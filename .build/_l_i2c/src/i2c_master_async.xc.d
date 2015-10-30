76d86fad36859c54fd32a36905c631950026d097
benallen <external@benallen.info>
2015-10-30 13:47:03 +0000

    First commit

---
+++ .build/_l_i2c/src/i2c_master_async.xc.d
@@ -0,0 +1,5 @@
+.build/_l_i2c/src//i2c_master_async.xc.d ./.build/_l_i2c/src//i2c_master_async.xc.o ./.build/_l_i2c/src//i2c_master_async.xc.pca.xml: \
+  ../../CWK2/lib_i2c/src/i2c_master_async.xc \
+  ../../CWK2/lib_i2c/api/i2c.h \
+  ../../CWK2/lib_xassert/api/xassert.h
+
---
+++ .build/_l_i2c/src/i2c_master_single_port.xc.d
@@ -0,0 +1,5 @@
+.build/_l_i2c/src//i2c_master_single_port.xc.d ./.build/_l_i2c/src//i2c_master_single_port.xc.o ./.build/_l_i2c/src//i2c_master_single_port.xc.pca.xml: \
+  ../../CWK2/lib_i2c/src/i2c_master_single_port.xc \
+  ../../CWK2/lib_i2c/api/i2c.h \
+  ../../CWK2/lib_xassert/api/xassert.h
+
